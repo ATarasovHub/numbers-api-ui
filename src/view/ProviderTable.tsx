17db28aa40659e694927292113e43547a34b9c66
atarasov <slonikghg06@gmail.com>
2025-06-10 11:44:24 +0200

    Added collapsible table with country info

--- src/view/ProviderTable.tsx
+++ src/view/ProviderTable.tsx
@@ -1,7 +1,9 @@
 import React, {useEffect, useState} from 'react'
-import {Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
+import {Table, TableBody, TableCell, TableHead, TableRow, IconButton, Collapse, Box, Typography} from "@mui/material";
 import {CountryStats, NumberProvider} from "../utils/domain";
 import {isDefined} from "../utils/util";
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 interface CountryStatsTableProps {
     stats: CountryStats[]
@@ -9,28 +11,38 @@ interface CountryStatsTableProps {
 
 const CountryStatsTable: React.FC<CountryStatsTableProps> = ({stats}) => (
     <Table size="small" aria-label="country stats">
-
+        <TableHead>
+            <TableRow>
+                <TableCell>Country Id</TableCell>
+                <TableCell>Country Name</TableCell>
+                <TableCell>Total Numbers</TableCell>
+                <TableCell>Assigned Numbers</TableCell>
+                <TableCell>Not Assigned Numbers</TableCell>
+                <TableCell>Monthly Cost</TableCell>
+            </TableRow>
+        </TableHead>
+        <TableBody>
+            {stats.map((stat) => (
+                <TableRow key={stat.countryId}>
+                    <TableCell>{stat.countryId}</TableCell>
+                    <TableCell>{stat.countryName}</TableCell>
+                    <TableCell>{stat.totalNumbers}</TableCell>
+                    <TableCell>{stat.assignedNumbers}</TableCell>
+                    <TableCell>{stat.totalNumbers - stat.assignedNumbers}</TableCell>
+                    <TableCell>{stat.totalMonthlyCost}</TableCell>
+                </TableRow>
+            ))}
+        </TableBody>
     </Table>
 )
 
-export const ProviderTable: React.FC = () => {
-    const [providers, setProviders] = useState<NumberProvider[]>([])
-    const [open, setOpen] = React.useState(false);
+interface ProviderRowProps {
+    provider: NumberProvider,
+    key?: number
+}
 
-    useEffect(() => {
-        console.log("Hello")
-        fetch(`/provider`)
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error(`HTTP ${res.status}`);
-                }
-                return res.json();
-            })
-            .then((providers: NumberProvider[]) => setProviders(providers))
-            .catch(error => {
-                Error(error.message || 'Something went wrong');
-            });
-    }, [])
+const ProviderRow: React.FC<ProviderRowProps> = ({provider, key}) => {
+    const [open, setOpen] = useState(false);
 
     function checkStatus(deletedAt: string) {
         if (!isDefined(deletedAt)) {
@@ -44,58 +56,77 @@ export const ProviderTable: React.FC = () => {
         } else {
             return "Deleted"
         }
-
     }
+
+    return (
+        <React.Fragment>
+            <TableRow>
+                <TableCell>{provider.providerId}</TableCell>
+                <TableCell>{provider.providerName}</TableCell>
+                <TableCell>{checkStatus(provider.deletedAt)}</TableCell>
+                <TableCell>{provider.totalNumbers}</TableCell>
+                <TableCell>{provider.totalAssignedNumbers}</TableCell>
+                <TableCell>{provider.totalNumbers - provider.totalAssignedNumbers}</TableCell>
+                <TableCell>{provider.totalMonthlyCost}</TableCell>
+                <TableCell>
+                    <IconButton
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>}
+                    </IconButton>
+                </TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{paddingBottom: 0, paddingTop: 0}} colSpan={8}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box sx={{margin: 1}}>
+                            <Typography variant="h6" gutterBottom component="div">
+                                Country Statistics
+                            </Typography>
+                            <CountryStatsTable stats={provider.countryStats || []}/>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
+    );
+};
+
+export const ProviderTable: React.FC = () => {
+    const [providers, setProviders] = useState<NumberProvider[]>([])
+
+    useEffect(() => {
+        fetch(`/provider`)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`HTTP ${res.status}`);
+                }
+                return res.json();
+            })
+            .then((providers: NumberProvider[]) => setProviders(providers))
+            .catch(error => {
+                Error(error.message || 'Something went wrong');
+            });
+    }, [])
 
     return (
         <Table>
             <TableHead>
                 <TableRow>
-                    <TableCell>
-                        Provider Id
-                    </TableCell>
-                    <TableCell>
-                        Provider name
-                    </TableCell>
-                    <TableCell>
-                        Status
-                    </TableCell>
-                    {/*<TableCell>*/}
-                    {/*    Country Id*/}
-                    {/*</TableCell>*/}
-                    {/*<TableCell>*/}
-                    {/*    Country name*/}
-                    {/*</TableCell>*/}
-                    <TableCell>
-                        Total numbers
-                    </TableCell>
-                    <TableCell>
-                        Assigned numbers
-                    </TableCell>
-                    <TableCell>
-                        Not assigned numbers
-                    </TableCell>
-                    <TableCell>
-                        Total monthly cost
-                    </TableCell>
+                    <TableCell>Provider Id</TableCell>
+                    <TableCell>Provider name</TableCell>
+                    <TableCell>Status</TableCell>
+                    <TableCell>Total numbers</TableCell>
+                    <TableCell>Assigned numbers</TableCell>
+                    <TableCell>Not assigned numbers</TableCell>
+                    <TableCell>Total monthly cost</TableCell>
+                    <TableCell>Actions</TableCell>
                 </TableRow>
             </TableHead>
             <TableBody>
-                {providers.map(p => (
-                    <TableRow key={`provider-table-row-${p.providerId}`}>
-                        <TableCell>{p.providerId}</TableCell>
-                        <TableCell>{p.providerName}</TableCell>
-                        <TableCell>{checkStatus(p.deletedAt)}</TableCell>
-                        <TableCell>{p.totalNumbers}</TableCell>
-                        <TableCell>{p.totalAssignedNumbers}</TableCell>
-                        <TableCell>{p.totalNumbers - p.totalAssignedNumbers}</TableCell>
-                        <TableCell>{p.totalMonthlyCost}</TableCell>
-                        <TableCell>
-                            <IconButton size="small" onClick={() => toggleRow(provider.providerId)}>
-                            {isOpen ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                        </IconButton>
-                        </TableCell>
-                    </TableRow>
+                {providers.map(provider => (
+                    <ProviderRow key={provider.providerId} provider={provider}/>
                 ))}
             </TableBody>
         </Table>
