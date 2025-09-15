f95accbd0c0037d9e267b750888b4bf443dfc12c
atarasov <slonikghg06@gmail.com>
2025-09-15 10:17:35 +0200

    restructured project2

--- src/view/pages/ProviderOverview/index.tsx
+++ src/view/pages/ProviderOverview/index.tsx
@@ -134,7 +134,6 @@ export const ProviderOverview: React.FC = () => {
                                         ))}
                                 </TableRow>
 
-                                //Test
                             </TableHead>
                             <TableBody>
                                 {loading ? (
